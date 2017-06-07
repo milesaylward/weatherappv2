738979ce2c65723562c3bd0f13c75250e1b73050
milesaylward <miles.aylward@gmail.com>
2017-06-07 09:01:08 -0400

    mixed content fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,7 +18,7 @@ export const TextChange = (text) => {
 
 export const Search = (value) => {
   return (dispatch) => {
-    axios.get(`http://api.apixu.com/v1/forecast.json?key=${key}&q=${value}&days=5`)
+    axios.get(`https://api.apixu.com/v1/forecast.json?key=${key}&q=${value}&days=5`)
       .then(res => {
         dispatch({ type: SEARCH_SUBMIT, payload: res.data })
       })
